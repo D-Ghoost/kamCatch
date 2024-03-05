3ab778110c48b375ffc49fc789f07097dae90434
Nicolas Torres Lop <nicolas.torreslop@gmail.com>
2024-03-05 01:33:22 -0500

    add: advance to dnd

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -3,11 +3,13 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import Login from "../Login";
+import Home from "../Home";
 
 
 const AppRouter = createBrowserRouter([
   { path: "/", element: <Login /> },
   { path: "/login", element: <Login /> },
+  { path: "/home", element: <Home /> },
 ]);
 
 function App() {
--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"; 
 import { UserCircleIcon } from "@heroicons/react/24/solid";
 import LayoutCenterItems from "../../Layout/LayoutCenterItems";
 
@@ -10,10 +11,10 @@ const Login = () =>{
 
     return (
         <LayoutCenterItems>
-            <div className=" w-6/12 h-3/5 flex flex-col items-center bg-cyan-50 ">
+            <div className=" w-4/5 h-3/5 flex flex-col items-center bg-cyan-50 md:w-6/12  ">
                 <UserCircleIcon className=" w-40 h-40 text-cyan-800 mt-4" />
-                <h1 className=" mt-4 text-3xl font-medium text-black ">Bienvenido a KamCatch</h1>
-                <form onSubmit={ validateInfo } className="mt-4" >
+                <h1 className=" mt-4 text-3xl font-medium text-black text-center ">Bienvenido a KamCatch</h1>
+                <form onSubmit={ validateInfo } className="mt-4 flex flex-col" >
                         <input 
                             type="text" 
                             placeholder="Usuario" 
@@ -26,12 +27,14 @@ const Login = () =>{
                             className=" w-full h-10 mt-4 border-2 hover:border-cyan-600 focus:outline-none focus:border-cyan-600 rounded-md mt-2 pl-3 italic" 
                             required
                         />
-                        <button 
-                            type="submit" 
-                            className=" w-full h-10 mt-4 bg-cyan-800 text-white rounded-md mt-2"
-                        >
-                            Iniciar Sesión
-                        </button>
+                        <Link to="/home">
+                            <button 
+                                type="submit" 
+                                className=" w-full h-10 mt-4 bg-cyan-800 text-white rounded-md mt-2"
+                            >
+                                Iniciar Sesión
+                            </button>
+                        </Link>
                 </form>
                 <hr className=" w-full h-2 mt-20 " />
                 <div className=" flex justify-center items-center w-full h-12 ">
